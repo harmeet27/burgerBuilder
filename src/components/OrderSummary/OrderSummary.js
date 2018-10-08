685d2a1040c216ef88608107ba28316d003bd8ab
harmeetkaur <harmeetkaur@dbs.com>
2018-10-08 17:51:11 +0530

    commit

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React , { Component } from  'react';
 import Aux from '../../hoc/Aux'
 import Button from '../Ui/Button/Button'
 
-const OrderSummary = (props) => {
-    const ingredientSummary  = 
-    Object.keys(props.igredients).map(igKey => {
-        return <li key={igKey}><span style={{textTransform : 'capitalize'}}>{igKey}</span> : {props.igredients[igKey]}</li>
+class OrderSummary extends Component {
+
+
+
+render(){
+  const ingredientSummary  = 
+    Object.keys(this.props.igredients).map(igKey => {
+        return <li key={igKey}><span style={{textTransform : 'capitalize'}}>{igKey}</span> : {this.props.igredients[igKey]}</li>
     })
-   return(
-       <Aux>
+
+        return (<Aux>
            <h3>Your Order</h3>
            <p>A Delicious burger with following ingridients:</p>
            <ul>
                {ingredientSummary}
            </ul>
-           <p><strong>Total Price:{props.totalPrice.toFixed(2)}</strong></p>
+           <p><strong>Total Price:{this.props.totalPrice.toFixed(2)}</strong></p>
            <p>Continue to checkout ?</p>
-           <Button buttonType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-           <Button buttonType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
+           <Button buttonType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+           <Button buttonType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
         </Aux>
-   );
+        );
+}
+
+
 
 }
 
